c5765aa654713ef6aed539e2c5c053e8ca379381
Ivan Milivojevic <seltik89@hotmail.com>
2020-03-04 17:18:25 +0100

    First table finished with column layout in JSON

--- src/app.js
+++ src/app.js
@@ -1,3 +1,47 @@
+class TableCreator {
+	static tableHead(data, columnBased, firstColumnTitle) {
+		const tableHead = document.createElement("thead");
+		const tableHeadRow = document.createElement("tr");
+
+		if (columnBased) {
+			const th = document.createElement("th");
+			th.textContent = firstColumnTitle;
+			tableHeadRow.appendChild(th);
+		}
+		for (const columnTitle in data) {
+			const th = document.createElement("th");
+			th.textContent = columnTitle;
+			tableHeadRow.appendChild(th);
+		}
+		tableHead.appendChild(tableHeadRow);
+
+		return tableHead;
+	}
+	
+	static tableBody(data, columnBased) {
+		const tableBody = document.createElement("tbody");
+		const bodyRowNames = Object.keys(data[Object.keys(data)[0]]);
+		const bodyRowCount = bodyRowNames.length;
+
+		for (let i = 0; i < bodyRowCount; i++) {
+			const tr = document.createElement("tr");
+			const tdname = document.createElement("td");
+			tdname.textContent = bodyRowNames[i];
+			tr.appendChild(tdname);
+
+			for (const column in data) {
+				const td = document.createElement("td");
+				td.textContent = Object.values(data[column])[i];
+				tr.appendChild(td);
+			}
+
+			tableBody.appendChild(tr);
+		}
+		
+		return tableBody;
+	}
+}
+
 class Table {
 	constructor(table, url, renderPlaceId, columnBased, firstColumnTitle) {
 		this.id = renderPlaceId;
@@ -22,21 +66,11 @@ class Table {
 		});
 	}
 	createTable(data, columnBased, firstColumnTitle) {
-		const tableHead = document.createElement("thead");
+		const tableHead = TableCreator.tableHead(data, columnBased, firstColumnTitle);
+		const tableBody = TableCreator.tableBody(data, columnBased);
 
-		const tableHeadRow = document.createElement("tr");
-		if (columnBased) {
-      const th = document.createElement("th");
-      th.textContent = firstColumnTitle;
-      tableHeadRow.appendChild(th);
-    }
-    for (const columnTitle in data) {
-      const th = document.createElement("th");
-      th.textContent = columnTitle;
-      tableHeadRow.appendChild(th);
-    }
-    tableHead.appendChild(tableHeadRow);
-    document.getElementById(this.id).appendChild(tableHead);
+		document.getElementById(this.id).appendChild(tableHead);
+		document.getElementById(this.id).appendChild(tableBody);
 	}
 }
 
