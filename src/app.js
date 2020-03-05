a59c3540112099351b0f8d6ef630faee69b05857
Ivan Milivojevic <seltik89@hotmail.com>
2020-03-05 01:20:44 +0100

    Created table based on row template

--- src/app.js
+++ src/app.js
@@ -1,43 +1,71 @@
 class TableCreator {
-	static tableHead(data, columnBased, firstColumnTitle) {
+	static tableUpdate() {
+		console.log(this, event.target);
+	}
+	static tableHead(data, columnBased, firstColumnTitle, tableObject) {
 		const tableHead = document.createElement("thead");
 		const tableHeadRow = document.createElement("tr");
 
 		if (columnBased) {
-			const th = document.createElement("th");
-			th.textContent = firstColumnTitle;
-			tableHeadRow.appendChild(th);
+			const thfirst = document.createElement("th");
+			thfirst.textContent = firstColumnTitle;
+			tableHeadRow.appendChild(thfirst);
+
+			for (const columnTitle in data) {
+				const th = document.createElement("th");
+				th.textContent = columnTitle;
+				tableHeadRow.appendChild(th);
+			}
+		} else {
+			for (const columnTitle in data[0]) {
+				const th = document.createElement("th");
+				th.textContent = columnTitle;
+				th.classList.add(columnTitle);
+				th.addEventListener("click", TableCreator.tableUpdate.bind(tableObject));
+				tableHeadRow.appendChild(th);
+			}
 		}
-		for (const columnTitle in data) {
-			const th = document.createElement("th");
-			th.textContent = columnTitle;
-			tableHeadRow.appendChild(th);
-		}
+
 		tableHead.appendChild(tableHeadRow);
 
 		return tableHead;
 	}
-	
+
 	static tableBody(data, columnBased) {
 		const tableBody = document.createElement("tbody");
-		const bodyRowNames = Object.keys(data[Object.keys(data)[0]]);
-		const bodyRowCount = bodyRowNames.length;
 
-		for (let i = 0; i < bodyRowCount; i++) {
-			const tr = document.createElement("tr");
-			const tdname = document.createElement("td");
-			tdname.textContent = bodyRowNames[i];
-			tr.appendChild(tdname);
+		if (columnBased) {
+			const bodyRowNames = Object.keys(data[Object.keys(data)[0]]);
+			const bodyRowCount = bodyRowNames.length;
 
-			for (const column in data) {
-				const td = document.createElement("td");
-				td.textContent = Object.values(data[column])[i];
-				tr.appendChild(td);
+			for (let i = 0; i < bodyRowCount; i++) {
+				const tr = document.createElement("tr");
+				const tdname = document.createElement("td");
+				tdname.textContent = bodyRowNames[i];
+				tr.appendChild(tdname);
+
+				for (const column in data) {
+					const td = document.createElement("td");
+					td.textContent = Object.values(data[column])[i];
+					tr.appendChild(td);
+				}
+
+				tableBody.appendChild(tr);
 			}
+		} else {
+			for (const row of data) {
+				const tr = document.createElement("tr");
 
-			tableBody.appendChild(tr);
+				for (const key in row) {
+					const td = document.createElement("td");
+					td.textContent = row[key];
+					tr.appendChild(td);
+				}
+
+				tableBody.appendChild(tr);
+			}
 		}
-		
+
 		return tableBody;
 	}
 }
@@ -66,17 +94,27 @@ class Table {
 		});
 	}
 	createTable(data, columnBased, firstColumnTitle) {
-		const tableHead = TableCreator.tableHead(data, columnBased, firstColumnTitle);
+		const tableHead = TableCreator.tableHead(data, columnBased, firstColumnTitle, this);
 		const tableBody = TableCreator.tableBody(data, columnBased);
 
-		document.getElementById(this.id).appendChild(tableHead);
-		document.getElementById(this.id).appendChild(tableBody);
+		const table = document.createElement("table");
+		table.classList.add("table");
+		table.appendChild(tableHead);
+		table.appendChild(tableBody);
+		document.getElementById(this.id).appendChild(table);
 	}
 }
 
 class App {
 	static init() {
 		this.dashboard = new Table("activities", "assets/json/dashboard.json", "dashboard-table", true, "Activity");
+		this.gamesSummary = new Table(
+			"gameStatisticsPerGame",
+			"assets/json/statistic-games-summary.json",
+			"games-summary-table",
+			false,
+			false
+		);
 	}
 }
 
