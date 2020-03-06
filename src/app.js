937c2b538434da7ed61b55473d9eac1061bc0c04
Ivan Milivojevic <seltik89@hotmail.com>
2020-03-06 01:08:43 +0100

    Added table sorting functionality for tables

--- src/app.js
+++ src/app.js
@@ -1,7 +1,36 @@
-class TableCreator {
-	static tableUpdate() {
-		console.log(this, event.target);
+class TableModifier {
+	static tableSort(tableObject, th) {
+		console.time();
+		const sortMethod = th.dataset.sortMethod;
+		const sortType = sortMethod === "gameName" || sortMethod === "currency" ? "text" : "number";
+		let sortDirection;
+		tableObject.sortState = sortMethod;
+		tableObject.tableData.sort(TableModifier.compare(sortMethod, sortType));
+		const unsortedTable = document.getElementById(tableObject.id).querySelector("tbody");
+		const sortedTable = TableCreator.tableBody(tableObject.tableData);
+		unsortedTable.replaceWith(sortedTable);
+		console.timeEnd();
 	}
+
+	static compare(sortMethod, sortType) {
+		return function innerSort(a, b) {
+			const valueA =
+				sortType === "number" ? parseFloat(a[sortMethod].toString().replace(/,/g, "")) : a[sortMethod].toUpperCase();
+			const valueB =
+				sortType === "number" ? parseFloat(b[sortMethod].toString().replace(/,/g, "")) : b[sortMethod].toUpperCase();
+
+			let comparison = 0;
+			if (valueA > valueB) {
+				comparison = 1;
+			} else if (valueA < valueB) {
+				comparison = -1;
+			}
+			return comparison;
+		};
+	}
+}
+
+class TableCreator {
 	static tableHead(data, columnBased, firstColumnTitle, tableObject) {
 		const tableHead = document.createElement("thead");
 		const tableHeadRow = document.createElement("tr");
@@ -20,8 +49,8 @@ class TableCreator {
 			for (const columnTitle in data[0]) {
 				const th = document.createElement("th");
 				th.textContent = columnTitle;
-				th.classList.add(columnTitle);
-				th.addEventListener("click", TableCreator.tableUpdate.bind(tableObject));
+				th.setAttribute("data-sort-method", columnTitle);
+				th.addEventListener("click", TableModifier.tableSort.bind(null, tableObject, th));
 				tableHeadRow.appendChild(th);
 			}
 		}
@@ -68,14 +97,32 @@ class TableCreator {
 
 		return tableBody;
 	}
+
+	static tableFoot(dataSum) {
+		const tableFoot = document.createElement("tfoot");
+		const tr = document.createElement("tr");
+
+		for (const key in dataSum[0]) {
+			const td = document.createElement("td");
+			td.textContent = dataSum[0][key];
+			tr.appendChild(td);
+		}
+
+		tableFoot.appendChild(tr);
+
+		return tableFoot;
+	}
 }
 
 class Table {
-	constructor(table, url, renderPlaceId, columnBased, firstColumnTitle) {
+	constructor(table, url, renderPlaceId, columnBased, firstColumnTitle, tableSum) {
 		this.id = renderPlaceId;
 		this.getData(url).then(data => {
 			this.tableData = data.result[table];
-			this.createTable(this.tableData, columnBased, firstColumnTitle);
+			if (tableSum) {
+				this.tableDataSum = [data.result[tableSum]];
+			}
+			this.createTable(this.tableData, columnBased, firstColumnTitle, this.tableDataSum);
 		});
 	}
 	getData(url) {
@@ -93,28 +140,36 @@ class Table {
 			xhr.send();
 		});
 	}
-	createTable(data, columnBased, firstColumnTitle) {
+	createTable(data, columnBased, firstColumnTitle, dataSum) {
 		const tableHead = TableCreator.tableHead(data, columnBased, firstColumnTitle, this);
 		const tableBody = TableCreator.tableBody(data, columnBased);
-
 		const table = document.createElement("table");
 		table.classList.add("table");
 		table.appendChild(tableHead);
 		table.appendChild(tableBody);
+
+		if(dataSum) {
+			const tableFoot = TableCreator.tableFoot(dataSum);
+			table.appendChild(tableFoot);
+		}
+		
 		document.getElementById(this.id).appendChild(table);
 	}
 }
 
 class App {
 	static init() {
+		console.time();
 		this.dashboard = new Table("activities", "assets/json/dashboard.json", "dashboard-table", true, "Activity");
-		this.gamesSummary = new Table(
+		const gamesSummary = new Table(
 			"gameStatisticsPerGame",
 			"assets/json/statistic-games-summary.json",
 			"games-summary-table",
 			false,
-			false
+			false,
+			"gameStatisticsSum"
 		);
+		console.timeEnd();
 	}
 }
 
